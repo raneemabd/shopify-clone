4ce175272a03a0dec120187ca254f1a71bd2c3f2
Raneem Abdullah <raneemabdullah19@gmail.com>
2023-02-01 22:05:18 +0200

    adding dist

---
+++ assets/VContainer.7e8558d9.js
@@ -0,0 +1,1 @@
+/* empty css              */import{m as t,u as n}from"./VBtn.ddeb3513.js";import{i as r,c as s}from"./index.c04da044.js";const f=r({name:"VContainer",props:{fluid:{type:Boolean,default:!1},...t()},setup(e,a){let{slots:o}=a;return n(()=>s(e.tag,{class:["v-container",{"v-container--fluid":e.fluid}]},o)),{}}});export{f as V};
